1a910d579d886835270bfe4bc0de316dbc082a5f
DeathTheBest <goosesuka@gmail.com>
2020-09-25 18:05:39 +0300

    nom nom nom
    nom nom nom

---
+++ StartButtonScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  // менять сцены вешать на кнопки или условия хп =< нуля еще что-то 
+
+public class StartButtonScript : MonoBehaviour
+{   
+    public string loadscene ="";
+
+    public void onClick()
+    {
+        SceneManager.LoadScene(loadscene, LoadSceneMode.Single);
+    }
+}
+
+// менять сцены вешать на кнопки или условия хп =< нуля еще что-то 
--- player.cs
+++ player.cs
@@ -3,15 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class player : MonoBehaviour   // не понятная хуйня которая генерится сама при создании скрипта юнити ( нет подтягивает библеотеку для подсветки кода итд ) 
 {
     
         
-    public float movementSpeed = 1.0f;              // тут все понятно скорость бега бла бла
+    public float movementSpeed = 1.0f;   // тут все понятно скорость бега бла бла добавляем возможность изменять скорость игроку в юнити  https://prnt.sc/unluym
     public float movementSpeedRightLeft = 1.0f;  // тут все понятно скорость влево вправо бла бла
     public FixedJoystick joystick;   // добавляем возможность в юнити прилепить скрипт для управления 
-    private int life = 100;     // скрытая переменная указаывет количество ХП персонажа или монстра объекта 
+    private int life = 100;     // скрытая переменная указывет количество ХП персонажа или монстра объекта ( не путать с хп полоской)
     public Text life_state;   // добавляем хп над игроком запихиваем в конвас текст изменяем ротацию (Y) на 90 градусов
 
     // change speed
@@ -25,16 +26,24 @@ public class player : MonoBehaviour   // не понятная хуйня кот
     // Update is called once per frame
     void Update()   
     {
-        transform.position +=  new Vector3(movementSpeed * Time.deltaTime, 0, -(joystick.Direction.x * movementSpeedRightLeft * Time.deltaTime));    
-        // change speed
+        transform.position +=  new Vector3(movementSpeed * Time.deltaTime, 0, -(joystick.Direction.x * movementSpeedRightLeft * Time.deltaTime));
+        // change speed  
     }
     public void updateLife(int puntos)    
     {
         life += puntos;
         print("Current life =" + life);
-        life_state.text = life.ToString();      // добавляем возможность изменять скорость игроку в юнити  https://prnt.sc/unluym
+        life_state.text = life.ToString();      
+        isAlive();
     }
     
+    private void isAlive()
+    {
+        if(life <=0 )
+        {
+            SceneManager.LoadScene("GameOverScene", LoadSceneMode.Single);
+        }
+    }
 }
 
 // надо найти как поварачивать персонажа всего вместе с камерой влево в право выключить постоянный бег 
