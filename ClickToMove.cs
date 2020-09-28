58b90ae17bd2eeb49ba052ff3288d4e7472e4028
DeathTheBest <goosesuka@gmail.com>
2020-09-28 10:41:23 +0300

    ClickToMove
    мойо мморпг близко О_о

---
+++ ClickToMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class ClickToMove : MonoBehaviour
+{
+    private Animator mAnimator;
+
+    private NavMeshAgent mNavMeshAgent;
+
+    private bool mRunning = false;
+
+    void Start()
+    {
+        mAnimator = GetComponent<Animator>();
+        mNavMeshAgent = GetComponent<NavMeshAgent>();
+
+
+    }
+
+    
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(ray,  out hit, 100))
+            {
+                mNavMeshAgent.destination = hit.point;
+            }
+        }
+
+        if(mNavMeshAgent.remainingDistance <= mNavMeshAgent.stoppingDistance)
+        {
+            mRunning = false;
+        }
+        else
+        {
+            mRunning = true;
+        }
+
+        mAnimator.SetBool("running", mRunning);
+
+    }
+}
