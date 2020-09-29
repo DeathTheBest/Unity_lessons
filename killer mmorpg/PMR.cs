9df30164a2fac04ff3b33c2a5403fb79ddd41cec
DeathTheBest <goosesuka@gmail.com>
2020-09-29 23:40:54 +0300

    kill all mmorpg xD
    https://www.youtube.com/watch?v=R7CUzpERCIY&list=PLqO-Zs8tURcBIX60mCdxqluJufXgq573W&index=1

---
+++ killer mmorpg/PMR.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PMR : MonoBehaviour
+{
+
+    public float timer;
+
+
+
+    void Update()
+    {
+        timer -= 1 * Time.deltaTime;
+
+        if (timer <= 0)
+            Destroy(this.gameObject);
+        
+    }
+
+    public void DestroyPMR()
+    {
+        Destroy (this.gameObject);
+       
+    }
+
+    
+}
---
+++ killer mmorpg/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Variables
+    public float movementSpeed;
+    
+    public GameObject playerMovePoint;
+    private Transform pmr;
+    private bool  pmrSpawned;
+    private GameObject triggeringPMR;
+
+    private bool moving;
+    
+
+
+
+    //Functions
+    void Update()
+    {
+
+        //player movement
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitDistance = 0.0f;
+
+        if (playerPlane.Raycast(ray, out hitDistance)) 
+        {
+            Vector3 mousePosition = ray.GetPoint(hitDistance);
+            //Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            if (Input.GetMouseButtonDown(0))    
+            {
+                moving = true;
+
+                if(pmrSpawned)
+                {
+                    pmr = null;
+                    pmr = Instantiate(playerMovePoint.transform, mousePosition, Quaternion.identity);
+                } else {
+                    pmr = Instantiate(playerMovePoint.transform, mousePosition, Quaternion.identity);
+                }
+            }
+        }
+        if (pmr)
+            pmrSpawned = true;
+        else
+            pmrSpawned = false;
+
+        if (moving)
+            Move();     
+    }
+    public void Move()
+    {   if(pmr) 
+        { 
+            transform.position = Vector3.MoveTowards(transform.position, pmr.transform.position, movementSpeed);
+        this.transform.LookAt(pmr.transform);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "PMR")
+        {
+            triggeringPMR = other.gameObject;
+            triggeringPMR.GetComponent<PMR>().DestroyPMR();
+        }
+    }
+}
